49f4f307df6d516279fc834c80d9f49d4ded70cb
Ivan <panferov.ia@phystech.edu>
2021-04-19 23:01:04 +0300

    Version 2.0

--- tree.cpp
+++ tree.cpp
@@ -3,134 +3,68 @@
 #include <assert.h>
 #include <string.h>
 
-typedef struct Tree{
+struct Tree;
+void add_node(char* str, Tree*& tree_);
+void tree_walk(Tree* tree_, void (*func)(Tree*));
+void free_(Tree* tree_);
+void print_(Tree* tree_);
 
-	int elem_    = 0;
+struct Tree{
+	char* node_  = nullptr;
 	Tree* left_  = nullptr;
 	Tree* right_ = nullptr;
-
-	
-
-} tree;
-
-void add_note(const int& elem, tree*& tree_);
-void tree_walk(tree* tree_);
+};
 
 int main(){
 
-	tree* tree_ = nullptr;
+	Tree* tree_ = (Tree*)calloc(1, sizeof(Tree));
 
-	int elem = 0;
+	char str[128];
 
-	while(true){
-		if(scanf("%d", &elem) != EOF){
-			add_note(elem, tree_);
-		} else {
-			break;
-		}
-	}
+ 	add_node(str, tree_);
+	
+	tree_walk(tree_, print_);
 
-	tree_walk(tree_);
+	tree_walk(tree_, free_);
 
 	return 0;
 }
 
-void add_note(const int& elem, tree*& tree_){
-	if(tree_ == nullptr){
-		tree_ = (tree*)calloc(1, sizeof(tree));
-		tree_->elem_ = elem;
-		return;
-	} else if(tree_->elem_ < elem){
-		add_note(elem, tree_->right_);
-	} else if(tree_->elem_ >= elem){
-		add_note(elem, tree_->left_);
-	}
-	return;
+void print_(Tree* tree_){
+	printf("%s\n", tree_->node_);
 }
 
-void tree_walk(tree* tree_){
+void free_(Tree* tree_){
+	free(tree_->node_);
+	free(tree_);
+}
+
+void tree_walk(Tree* tree_, void (*func)(Tree*)){
 	if(tree_ == nullptr){
 		return;
 	} else if(tree_->left_ != nullptr){
 
-		tree_walk(tree_->left_);
+		tree_walk(tree_->left_, func);
 		
 		if(tree_->right_ != nullptr){
-			tree_walk(tree_->right_);
+			tree_walk(tree_->right_, func);
 		}
-
-		printf("%d\n", tree_->elem_);
-		free(tree_);
-	
+		func(tree_);
 	} else if(tree_->right_ != nullptr){
 
-		tree_walk(tree_->right_);
+		tree_walk(tree_->right_, func);
 		
 		if(tree_->left_ != nullptr){
-			tree_walk(tree_->left_);
-		}
-
-		printf("%d\n", tree_->elem_);
-		free(tree_);
-	
+			tree_walk(tree_->left_, func);
+		} 
+		func(tree_);
 	} else {
-
-		printf("%d\n", tree_->elem_);
-		free(tree_);	
+		func(tree_);
 	}
 }
 
-void func(tree* tree_){
+void add_node(Tree*& tree_){
 
-	assert(tree_);
+	
 
-	char str[128] = {};
-
-	tree* curr_node = tree_;
-
-	while(true){
-
-		if(curr_node != nullptr){
-			printf("%s?\n", curr_node->elem_);
-			scanf("%s", str);
-			if(!strcmp(str, "Yes")){
-				curr_node = curr_node->right_;
-				for(int i = 0; i < 3; i++){
-					str[i] = 0;
-				}
-			} else if(!strcmp(str, "No")){
-				curr_node = curr_node->left_;
-				for(int i = 0; i < 2; i++){
-					str[i] = 0;
-				}
-			} else {
-				printf("Couldn't understand, please type Yes or No\n");
-				scanf("%s", str);
-				for(int i = 0; i < 128; i++){
-					str[i] = 0;
-				}
-			}
-		} else {
-			printf("%s?\n", curr_node->elem_);
-			scanf("%s", str);
-			if(!strcmp(str, "Yes")){
-				printf("Wohooo");
-				for(int i = 0; i < 3; i++){
-					str[i] = 0;
-				}
-
-			} else if(!strcmp(str, "No")){
-				printf("Fuck");
-				for(int i = 0; i < 3; i++){
-					str[i] = 0;
-				}
-			} else {
-				printf("Couldn't understand, please type Yes or No\n");
-				scanf("%s", str);
-				for(int i = 0; i < 128; i++){
-					str[i] = 0;
-				}
-			}
-		}
-	} 
 }
